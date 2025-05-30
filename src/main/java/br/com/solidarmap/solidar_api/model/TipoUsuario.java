45aa8d371723a15bca192f3c015d2a31474f002f
P3terHenry <pedro.antonieti@outlook.com>
2025-05-30 19:47:24 -0300

    ✨ Adicionada a entidade TipoUsuario com mapeamento JPA.

---
+++ src/main/java/br/com/solidarmap/solidar_api/model/TipoUsuario.java
@@ -0,0 +1,24 @@
+package br.com.solidarmap.solidar_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "T_SMP_TIPO_USUARIOS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TipoUsuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_TIPO_USUARIO")
+    private Long id;
+
+    @Column(name = "NOME_TIPO")
+    private String nomeTipo;
+}
