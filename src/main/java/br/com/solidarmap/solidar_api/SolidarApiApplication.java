fd6b1ee9636df0021578dbab99e0680607c3a432
P3terHenry <pedro.antonieti@outlook.com>
2025-05-30 15:56:57 -0300

    ✨ Configurações iniciais do Spring Boot: adicionados @EnableCaching, @EnableJpaRepositories, @ComponentScan e @EntityScan.

--- src/main/java/br/com/solidarmap/solidar_api/SolidarApiApplication.java
+++ src/main/java/br/com/solidarmap/solidar_api/SolidarApiApplication.java
@@ -2,7 +2,13 @@ package br.com.solidarmap.solidar_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableCaching
+@EnableJpaRepositories
+@EntityScan
 @SpringBootApplication
 public class SolidarApiApplication {
 
