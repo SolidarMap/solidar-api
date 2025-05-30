e1495f75cd87c030f8e661c5b304861a7ab35d4a
P3terHenry <pedro.antonieti@outlook.com>
2025-05-30 10:42:26 -0300

    🎉 Início do projeto.

---
+++ src/main/java/br/com/solidarmap/solidar_api/SolidarApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.solidarmap.solidar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/solidarmap/solidar_api/SolidarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solidarmap.solidar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
