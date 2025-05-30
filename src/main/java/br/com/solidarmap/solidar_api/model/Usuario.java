d9db4de80f2b4f59b70681fc4bef0c2dd4bb1ada
P3terHenry <pedro.antonieti@outlook.com>
2025-05-30 20:05:08 -0300

    ✨ Adicionada a entidade Usuario com mapeamento JPA.

---
+++ src/main/java/br/com/solidarmap/solidar_api/model/Usuario.java
@@ -0,0 +1,40 @@
+package br.com.solidarmap.solidar_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "T_SMP_USUARIOS")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_USUARIO")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_TIPO_USUARIO")
+    private TipoUsuario tipoUsuario;
+
+    @Column(name = "NOME")
+    private String nome;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "SENHA")
+    private String senha;
+
+    @Column(name = "DATA_CRIACAO")
+    private LocalDateTime dataCriacao;
+
+}
