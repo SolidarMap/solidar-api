4539fd1bdc2c652adcb28eed8de7eaf2892eeef0
P3terHenry <pedro.antonieti@outlook.com>
2025-05-30 21:49:01 -0300

    🔒️ Carregado variáveis de ambiente do Oracle usando a biblioteca Dotenv.

--- src/main/java/br/com/solidarmap/solidar_api/SolidarApiApplication.java
+++ src/main/java/br/com/solidarmap/solidar_api/SolidarApiApplication.java
@@ -1,5 +1,6 @@
 package br.com.solidarmap.solidar_api;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -13,6 +14,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class SolidarApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("ORACLE_URL", dotenv.get("ORACLE_URL"));
+		System.setProperty("ORACLE_USER", dotenv.get("ORACLE_USER"));
+		System.setProperty("ORACLE_PASSWORD", dotenv.get("ORACLE_PASSWORD"));
+		System.setProperty("ORACLE_DRIVER", dotenv.get("ORACLE_DRIVER"));
+
 		SpringApplication.run(SolidarApiApplication.class, args);
 	}
 
