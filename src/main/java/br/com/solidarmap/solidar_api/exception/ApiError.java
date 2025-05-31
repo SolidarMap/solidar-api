faa0b8a7584ea55e9ad754ac938fa02e0ca302ed
P3terHenry <pedro.antonieti@outlook.com>
2025-05-31 18:45:01 -0300

    ✨ Adicionado tratamento global de exceções com `GlobalExceptionHandler` e classe auxiliar `ApiError`.

---
+++ src/main/java/br/com/solidarmap/solidar_api/exception/ApiError.java
@@ -0,0 +1,28 @@
+package br.com.solidarmap.solidar_api.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiError {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+
+    public ApiError(HttpStatus status, String message, String path) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status.value();
+        this.error = status.getReasonPhrase();
+        this.message = message;
+        this.path = path;
+    }
+}
