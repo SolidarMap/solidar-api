33bbc9d32e170cefd35863e4c74e3c9eb6ab69e0
P3terHenry <pedro.antonieti@outlook.com>
2025-05-31 18:43:30 -0300

    ✨ Adicionado endpoint `GET /usuario/todos` para listar todos os usuários com documentação Swagger.

--- src/main/java/br/com/solidarmap/solidar_api/controller/UsuarioController.java
+++ src/main/java/br/com/solidarmap/solidar_api/controller/UsuarioController.java
@@ -1,9 +1,33 @@
 package br.com.solidarmap.solidar_api.controller;
 
+import br.com.solidarmap.solidar_api.model.Usuario;
+import br.com.solidarmap.solidar_api.repository.UsuarioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/usuario")
 public class UsuarioController {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+    @Operation(summary = "Listar todos os usuários")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de usuários retornada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Nenhum usuário encontrado", content = @Content(schema = @Schema(hidden = true)))
+    })
+    @GetMapping("/todos")
+    public List<Usuario> retornaTodosUsuarios() {
+        return usuarioRepository.findAll();
+    }
 }
