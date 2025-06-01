e32824cfb4d18d4d119005392d54b14bc45afdb7
P3terHenry <pedro.antonieti@outlook.com>
2025-06-01 00:26:42 -0300

    ✨ Adicionado esquema de segurança JWT à configuração do Swagger para suporte à autenticação Bearer.

--- src/main/java/br/com/solidarmap/solidar_api/swagger/SwaggerConfiguration.java
+++ src/main/java/br/com/solidarmap/solidar_api/swagger/SwaggerConfiguration.java
@@ -1,5 +1,7 @@
 package br.com.solidarmap.solidar_api.swagger;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,6 +9,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfiguration {
 
     @Bean
