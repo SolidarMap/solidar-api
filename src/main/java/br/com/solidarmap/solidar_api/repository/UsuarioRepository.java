0239657c79e53c911af5670932b8e4216f719805
P3terHenry <pedro.antonieti@outlook.com>
2025-06-01 12:41:10 -0300

    ✨ Alterado `UsuarioConfig` para utilizar o banco de dados na autenticação de usuários, com implementação de método no `UsuarioRepository`.

--- src/main/java/br/com/solidarmap/solidar_api/repository/UsuarioRepository.java
+++ src/main/java/br/com/solidarmap/solidar_api/repository/UsuarioRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
@@ -44,4 +45,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     WHERE u.id = :id
     """)
     UsuarioDTO findUsuarioById(@Param("id") Long id);
+
+    @Query("SELECT u FROM Usuario u WHERE u.email = :email")
+    Optional<Usuario> findByEmailforAuth(String email);
 }
--- src/main/java/br/com/solidarmap/solidar_api/security/UsuarioConfig.java
+++ src/main/java/br/com/solidarmap/solidar_api/security/UsuarioConfig.java
@@ -1,21 +1,39 @@
 package br.com.solidarmap.solidar_api.security;
 
+import br.com.solidarmap.solidar_api.model.Usuario;
+import br.com.solidarmap.solidar_api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class UsuarioConfig {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Bean
     UserDetailsService gerarUsuario() {
 
-        return new InMemoryUserDetailsManager(User
-                .withUsername("pedro.antonieti@gmail.com")
-                .password("{noop}pedro1971")
-                .roles("Admin")
-                .build());
+       return email -> {
+           Usuario usuario = usuarioRepository.findByEmailforAuth(email)
+                   .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado."));
+
+           return User.builder()
+                   .username(usuario.getEmail())
+                   .password(usuario.getSenha())
+                   .roles(usuario.getTipoUsuario().getNomeTipo())
+                   .build();
+       };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
