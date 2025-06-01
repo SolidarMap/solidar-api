8f514d2bf11585ff3869a86b5dcb91e894f4d0da
P3terHenry <pedro.antonieti@outlook.com>
2025-06-01 00:25:02 -0300

    ✨ Adicionado utilitário `JWTUtil` para geração, validação e extração de informações de tokens JWT.

---
+++ src/main/java/br/com/solidarmap/solidar_api/security/JWTUtil.java
@@ -0,0 +1,46 @@
+package br.com.solidarmap.solidar_api.security;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private final SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String construirToken(String email) {
+
+        Date data_atual = new Date();
+
+        JwtBuilder builder = Jwts.builder()
+                .subject(email)
+                .issuedAt(data_atual)
+                .expiration(new Date(data_atual.getTime() + (3600000)))
+                .signWith(secretKey);
+        return builder.compact();
+    }
+
+    public String extrairEmail(String token) {
+
+        JwtParser parser = Jwts.parser().verifyWith(secretKey).build();
+
+        return parser.parseSignedClaims(token).getPayload().getSubject();
+    }
+
+    public boolean validarToken(String token) {
+        try {
+            JwtParser parser = Jwts.parser().verifyWith(secretKey).build();
+            parser.parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
