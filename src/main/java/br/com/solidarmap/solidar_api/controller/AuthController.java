6df044b53c9ae7ac75b0143350beb368aef31d4d
P3terHenry <pedro.antonieti@outlook.com>
2025-06-01 01:32:30 -0300

    🔧 Adicionada documentação Swagger ao endpoint de login no `AuthController`

--- src/main/java/br/com/solidarmap/solidar_api/controller/AuthController.java
+++ src/main/java/br/com/solidarmap/solidar_api/controller/AuthController.java
@@ -1,6 +1,11 @@
 package br.com.solidarmap.solidar_api.controller;
 
 import br.com.solidarmap.solidar_api.security.JWTUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,13 +25,18 @@ public class AuthController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+
+    @Operation(summary = "Solicitar token de autenticação")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Token gerado com sucesso."),
+            @ApiResponse(responseCode = "401", description = "Credenciais inválidas.", content = @Content(schema = @Schema(hidden = true)))
+    })
     @PostMapping("/login")
     public String gerarToken(String email, String senha) {
         try {
             var authentication = new UsernamePasswordAuthenticationToken(email, senha);
             authenticationManager.authenticate(authentication);
             return jwtUtil.construirToken(email);
-
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Credenciais inválidas");
         }
