e68781e5a8426e780a3dbb0acbaf60840ee02607
P3terHenry <pedro.antonieti@outlook.com>
2025-06-02 03:30:02 -0300

    🔧 Ajustado `ApiError` para utilizar `HttpStatus` diretamente e adicionado formato personalizado para o campo `timestamp`.

--- src/main/java/br/com/solidarmap/solidar_api/exception/ApiError.java
+++ src/main/java/br/com/solidarmap/solidar_api/exception/ApiError.java
@@ -1,5 +1,6 @@
 package br.com.solidarmap.solidar_api.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,20 +9,19 @@ import org.springframework.http.HttpStatus;
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class ApiError {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime timestamp;
-    private int status;
-    private String error;
+
+    private HttpStatus status;
     private String message;
     private String path;
 
     public ApiError(HttpStatus status, String message, String path) {
         this.timestamp = LocalDateTime.now();
-        this.status = status.value();
-        this.error = status.getReasonPhrase();
+        this.status = status;
         this.message = message;
         this.path = path;
     }
