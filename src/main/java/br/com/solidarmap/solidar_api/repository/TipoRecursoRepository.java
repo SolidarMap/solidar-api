7cbb215b6f054e4aad5afecb00664a63d8cafa8b
P3terHenry <pedro.antonieti@outlook.com>
2025-06-03 03:35:37 -0300

    ✨ Adicionado método `existsByRecurso` no `TipoRecursoRepository` para verificar a existência de recursos por nome.

--- src/main/java/br/com/solidarmap/solidar_api/repository/TipoRecursoRepository.java
+++ src/main/java/br/com/solidarmap/solidar_api/repository/TipoRecursoRepository.java
@@ -2,6 +2,11 @@ package br.com.solidarmap.solidar_api.repository;
 
 import br.com.solidarmap.solidar_api.model.TipoRecurso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TipoRecursoRepository extends JpaRepository<TipoRecurso, Long> {
+
+    @Query("SELECT COUNT(t) > 0 FROM TipoRecurso t WHERE LOWER(t.recurso) = LOWER(:recurso)")
+    boolean existsByRecurso(@Param("recurso") String recurso);
 }
