d8123ff9ffea8de67d1086842bed1d0bda7dbfb2
P3terHenry <pedro.antonieti@outlook.com>
2025-06-06 16:36:40 -0300

    ✨ Atualizado o repositório `LocalizacaoRepository` para retornar DTOs em vez de entidades nas consultas por ID de ajuda e ID de localização.

--- src/main/java/br/com/solidarmap/solidar_api/repository/LocalizacaoRepository.java
+++ src/main/java/br/com/solidarmap/solidar_api/repository/LocalizacaoRepository.java
@@ -6,14 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
 
 public interface LocalizacaoRepository extends JpaRepository<Localizacao, Long> {
 
-    @Query("SELECT l FROM Localizacao l WHERE l.ajuda.id = :ajudaId")
+    @Query("SELECT new br.com.solidarmap.solidar_api.dto.LocalizacaoDTO(" +
+            "l.id, l.ajuda.id, l.tipoZona.id, l.latitude, l.longitude) " +
+            "FROM Localizacao l WHERE l.ajuda.id = :ajudaId")
     LocalizacaoDTO findLocalizacaoByAjudaId(@Param("ajudaId") Long ajudaId);
 
-    @Query("SELECT l FROM Localizacao l WHERE l.id = :id")
+    @Query("SELECT new br.com.solidarmap.solidar_api.dto.LocalizacaoDTO(" +
+            "l.id, l.ajuda.id, l.tipoZona.id, l.latitude, l.longitude) " +
+            "FROM Localizacao l WHERE l.id = :id")
     LocalizacaoDTO findByIdLocalizacao(@Param("id") Long id);
 
 
