5e09c2cc9d05d8c2c378f97e8113a29205d607c4
P3terHenry <pedro.antonieti@outlook.com>
2025-06-06 16:37:30 -0300

    ✨ Adicionada consulta para listar todas as ajudas e método para paginar ajudas por ID de usuário no repositório AjudaRepository.

--- src/main/java/br/com/solidarmap/solidar_api/repository/AjudaRepository.java
+++ src/main/java/br/com/solidarmap/solidar_api/repository/AjudaRepository.java
@@ -1,7 +1,21 @@
 package br.com.solidarmap.solidar_api.repository;
 
+import br.com.solidarmap.solidar_api.dto.AjudaDTO;
 import br.com.solidarmap.solidar_api.model.Ajuda;
+import br.com.solidarmap.solidar_api.model.Mensagem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AjudaRepository extends JpaRepository<Ajuda, Long> {
+
+    @Query(value = "SELECT * FROM T_SMP_AJUDAS", nativeQuery = true)
+    List<AjudaDTO> findAllAjudas();
+
+    @Query("SELECT a FROM Ajuda a WHERE a.usuario.id = :idUsuario")
+    Page<Ajuda> findAjudasByIdUsuario(@Param("idUsuario") Long idUsuario, Pageable pageable);
 }
