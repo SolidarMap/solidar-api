3602891170b5f1de42b698ca04dd0c9cb034c5e9
P3terHenry <pedro.antonieti@outlook.com>
2025-06-06 21:18:48 -0300

    ✨ Atualizada a consulta para listar ajudas, agora retornando AjudaDTO, e adicionada nova consulta para listar ajudas com mensagens e avaliações.

--- src/main/java/br/com/solidarmap/solidar_api/repository/AjudaRepository.java
+++ src/main/java/br/com/solidarmap/solidar_api/repository/AjudaRepository.java
@@ -5,6 +5,7 @@ import br.com.solidarmap.solidar_api.model.Ajuda;
 import br.com.solidarmap.solidar_api.model.Mensagem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,8 +14,22 @@ import java.util.List;
 
 public interface AjudaRepository extends JpaRepository<Ajuda, Long> {
 
-    @Query(value = "SELECT * FROM T_SMP_AJUDAS", nativeQuery = true)
+    @Query("""
+    SELECT new br.com.solidarmap.solidar_api.dto.AjudaDTO(
+        a.id,
+        a.usuario.id,
+        a.recurso,
+        a.descricao,
+        a.status,
+        a.dataPublicacao
+    )
+    FROM Ajuda a
+""")
     List<AjudaDTO> findAllAjudas();
+
+    @EntityGraph(attributePaths = {"mensagens", "avaliacoes"})
+    @Query("SELECT a FROM Ajuda a")
+    List<Ajuda> findAllComMensagensEAvaliacoes();
 
     @Query("SELECT a FROM Ajuda a WHERE a.usuario.id = :idUsuario")
     Page<Ajuda> findAjudasByIdUsuario(@Param("idUsuario") Long idUsuario, Pageable pageable);
